1b9c2c687bd7dc17557dac6368e8670447343cb4
AlsharifR <AlsharifR@maaden.com.sa>
2025-05-12 10:42:58 +0300

    Update auto_tracing_update.py

--- auto_tracing_update.py
+++ auto_tracing_update.py
@@ -55,8 +55,8 @@ def update_excel(file_path):
             df[col] = ""
 
     for i in range(len(df)):
-        container = str(df.loc[i, "ContainsNumber"]).strip() if not pd.isna(df.loc[i, "ContainsNumber"]) else ""
-        booking = str(df.loc[i, "BookingNumber"]).strip() if not pd.isna(df.loc[i, "BookingNumber"]) else ""
+        container = str(df.loc[i, "ContainsNumber"]).strip() if not pd.isna(df.loc[i, "ContainsNumber "]) else ""
+        booking = str(df.loc[i, "BookingNumber"]).strip() if not pd.isna(df.loc[i, "BookingNumber "]) else ""
 
         tracking_number = booking if booking else container
         if not tracking_number:
